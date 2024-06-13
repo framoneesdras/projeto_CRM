d7c2f02065b8a7ab1f51a83573f207da2b7f7bb3
framoneesdras <116281478+framoneesdras@users.noreply.github.com>
2024-06-13 20:06:03 -0300

    Update main.cpp

--- Projeto_Hotel/principal/main.cpp
+++ Projeto_Hotel/principal/main.cpp
@@ -27,11 +27,18 @@ mensagem_menu() {
     std::cout << "2 -> Opcao 2" << std::endl;
     std::cout << "3 -> Opcao 3" << std::endl;
     std::cout << "4 -> Sair" << std::endl;
+    std::cout << std::endl;
 }
 
 mensagem_teste() {
     std::cout << "TESTE TESTE TESTE" << std::endl;
 }
+
+converte_comando() {
+    std::cout << "CONVERTE";
+}
+
+using namespace std;
 
 
 int main() {
@@ -39,7 +46,7 @@ int main() {
     mensagem_inicial();
 
     bool i = true;
-    int escolha;
+    string escolha;
 
 
     while(i==true) {
@@ -47,26 +54,25 @@ int main() {
         mensagem_menu();
         std::cin >> escolha;
 
-        switch (escolha) {
-            case 1:
-                std::cout << "Escolheu 1";
+        switch (escolha)
+        {
+            case '1':
+                std::cout << "Escolheu 1" << endl << endl;
                 break;
-            case 2:
-                std::cout << "Escolheu 2";
+            case '2':
+                std::cout << "Escolheu 2" << endl << endl;
                 break;
-            case 3:
-                std::cout << "Escolheu 3";
+            case '3':
+                std::cout << "Escolheu 3" << endl << endl;
                 break;
-            case 4:
-                std::cout << "Escolheu Sair";
-                return 0;
+            case '4':
+                std::cout << "Escolheu Sair" << endl << endl;
+                i = false;
                 break;
             default:
-                std::cout << "Escolha invalida";
+                std::cout << "Escolha invalida" << endl << endl;
                 break;
         }
     }
     mensagem_teste();
-
-    return 0;
 }
