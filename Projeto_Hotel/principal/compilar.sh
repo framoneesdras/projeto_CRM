3e40209696a10d8bc577f1b13f20e0db2ee46add
framoneesdras <116281478+framoneesdras@users.noreply.github.com>
2024-06-16 16:52:57 -0300

    Update compilar.sh
    Compilando com os arquivos .h e .cpp em pastas diferentes

--- Projeto_Hotel/principal/compilar.sh
+++ Projeto_Hotel/principal/compilar.sh
@@ -1,30 +1,5 @@
 # Com o CMD aberto na pasta "Projeto_Hotel", no windows, siga os seguintes passos para compilar:
 
-cd include/
-g++ -c *.cpp 
-cd ..
-cd principal/
-g++ -c *.cpp
-cd ..
-g++ include/*.h principal/*.cpp -o executar_interface
-executar_interface.exe
-
-// para compilar, por enquanto, é preciso adicionar os arquivos de src em include. 
-
-
-g++ *.h *.cpp -o executar_interface
-
-
-#Com tudo na mesma pasta
-
-#Dados.csv tem que ficar junto ao exercutável
-
-g++ -c *.cpp
-g++ *.h *.cpp -o executar_interface
-executar_interface.exe
-
-#Nova tentantiva
-
 cd src/
 g++ -c *.cpp
 cd ..
